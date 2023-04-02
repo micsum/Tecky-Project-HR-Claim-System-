9e49b81b6b906aa15d6c4c2fe26916b94f62f820
dt29941 <darren.tse.career@gmail.com>
2023-04-02 17:13:09 +0800

    finish the logic of profile page

--- profile.ts
+++ profile.ts
@@ -11,11 +11,17 @@ profileRouter.use(express.json());
 profileRouter.get("/profile", async (req, res) => {
     res.redirect("profile.html");
     //console.log(req.session.user?.id)
+  });
+
+  profileRouter.get("/getProfile", async (req, res) => {
+    //console.log(req.session.user?.id)
     let dbChecking =  await client.query(
-        /*sql*/ `SELECT employee.id,employee.name, employee.email, employee.phone_number,employee.department_id, department.name as department_name FROM employee join department on department.id = employee.department_id WHERE employee.id=$1`,
+        /*sql*/ `SELECT department.id, department.name, employee.id,employee.name, employee.email, employee.role, employee.phone_number,employee.department_id, department.name as department_name FROM employee join department on department.id = employee.department_id WHERE employee.id=$1`,
         [req.session.user?.id]
       );
       let employeeList = dbChecking.rows[0]
       console.log(employeeList)
+    //console.log(employeeList.name)
       res.json(employeeList)
+      
   });
